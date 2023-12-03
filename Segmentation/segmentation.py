1884db3f2379fae762040b70590f62237254264d
doraviv02 <46030130+doraviv02@users.noreply.github.com>
2023-12-03 13:29:24 +0200

    start corner detection

--- Segmentation/segmentation.py
+++ Segmentation/segmentation.py
@@ -24,16 +24,22 @@ colors = np.random.randint(0, 255, size=(n_labels , 3), dtype=np.uint8)
 colors[0] = [0, 0, 0]  # for cosmetic reason we want the background black
 false_colors = colors[labels]
 
+# Perform close operation on binary_image
+binary_image = cv2.morphologyEx(binary_image, cv2.MORPH_CLOSE, np.ones((10, 10), np.uint8))
+binary_image = cv2.morphologyEx(binary_image, cv2.MORPH_RECT, np.ones((3, 3), np.uint8))
 cv2.imshow('binary', binary_image)
+cv2.imwrite('binary.png', binary_image)
 cv2.imshow('false_colors', false_colors)
 
 MIN_AREA = 50
 false_colors_draw = false_colors.copy()
+# Loop over the centroids and labels
 for i, centroid in enumerate(centroids[1:], start=1):
     area = stats[i, 4]
     if area > MIN_AREA:
         new_image = original_image.copy()
-        new_image = cv2.bitwise_and(new_image,new_image, mask= ((labels==i)*255).astype(np.uint8))
+        mask = ((labels==i)*255).astype(np.uint8)
+        new_image = cv2.bitwise_and(new_image,new_image, mask= mask)
         cv2.imshow("filtered " +str(i) ,new_image)
         #print(new_image[new_image[:,:,0]!=0][:,0])
         avg_red = np.average(new_image[new_image[:,:,1]!=0][:,1])
