c45495a1c3713e338bc47ab82334f9391efc4875
Uri-Sadan <urisy21@gmail.com>
2023-12-03 16:23:40 +0200

    initial_find top
    connecting the segmentation file to top_finder. modified both of them.

--- Segmentation/top_finder.py
+++ Segmentation/top_finder.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import ConvexHull
 import cv2
+from segmentation import segfunc
 
 def find_centroids(dst):
     ret, dst = cv2.threshold(dst, 0.01 * dst.max(), 255, 0)
@@ -17,60 +18,92 @@ def find_centroids(dst):
     return corners
 
 def up(points):
-    plt.figure()
-    plt.scatter(points[:, 0], points[:, 1])
-    plt.xlim([-1, 5])
-    plt.ylim([-1, 4])
-    plt.show()
+    #plt.figure()
+    #plt.scatter(points[:, 0], points[:, 1])
+    #plt.xlim([-1, 5])
+    #plt.ylim([-1, 4])
+    #plt.show()
 
-    lowest_point_idx = np.argmin(points[:, 0])
-    orderred_indecies = ConvexHull(points).vertices
-    orderred_points = points[orderred_indecies]
+    xorder = np.argsort(points[:,0])
+    xtemp = points[xorder]
 
-    left_to_lowest = (lowest_point_idx - 1) % len(points)
-    above_left = (left_to_lowest - 1) % len(points)
+    right = np.array([xtemp[4], xtemp[5]])
+    tr = right[np.argmin(right[:, 1])]
+    br = right[np.argmax(right[:, 1])]
 
-    offset = orderred_points[above_left] - orderred_points[left_to_lowest]
-    missing_point = orderred_points[lowest_point_idx] + offset
+    left = np.array([xtemp[0], xtemp[1]])
+    tl = left[np.argmin(left[:, 1])]
+    bl = left[np.argmax(left[:, 1])]
 
-    plt.figure()
-    plt.scatter(orderred_points[:, 0], orderred_points[:, 1])
-    plt.scatter(missing_point[0], missing_point[1])
-    plt.xlim([-1, 5])
-    plt.ylim([-1, 4])
-    plt.show()
+    right_offset = tr - br
+    left_offset = tl - bl
 
-    top_four = np.array([missing_point, orderred_points[above_left], orderred_points[(above_left+1)%len(points)], orderred_points[(above_left+2)%len(points)]])
+    mid_corners = np.delete(xtemp, [0, 1, 4, 5], axis=0)
+
+    lowest_mid = mid_corners[np.argmax(mid_corners[:,1])]
+    top_mid = mid_corners[np.argmin(mid_corners[:,1])]
+    lost_corner = lowest_mid + (np.array(right_offset) + np.array(left_offset))/2
+
+    #lowest_point_idx = np.argmin(points[:,0])
+    #orderred_indecies = ConvexHull(points).vertices
+    #orderred_points = points[orderred_indecies]
+
+    #left_to_lowest = (lowest_point_idx - 1) % len(points)
+    #above_left = (left_to_lowest - 1) % len(points)
+
+    #print(orderred_points)
+    #offset = orderred_points[above_left] - orderred_points[left_to_lowest]
+    #missing_point = orderred_points[lowest_point_idx] + offset
+
+    #plt.figure()
+    #plt.scatter(orderred_points[:, 0], orderred_points[:, 1])
+    #plt.scatter(missing_point[0], missing_point[1])
+    #plt.xlim([-1, 5])
+    #plt.ylim([-1, 4])
+    #plt.show()
+
+    top_four = np.array([lost_corner, tl, top_mid, tr])
 
     return top_four
 
-filename = './images/block.jpg'
+filename = 'test.png'
+seg_blocks = segfunc(filename)
 
-img = cv2.imread(filename)
-cv2.imshow('Original image', img)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-gray = np.float32(gray)
-dst = cv2.cornerHarris(gray, 2, 3, 0.0038)
-corners = find_centroids(dst)
-corners = np.delete(corners,0, axis=0)
-# To draw the corners
-for corner in corners:
-   cv2.circle(img,(int(corner[0]), int(corner[1])), 5, (255,0,0), -1)
-   #img[int(corner[1]), int(corner[0])] = [0, 0, 255]
-cv2.imshow('Found Corners', img)
+#running on each segmented block
+for img in seg_blocks:
+    # cv2.imshow('Original image', img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = np.float32(gray)
+    #dst = cv2.cornerHarris(gray, 2, 3, 0.04)
+    corners = cv2.goodFeaturesToTrack(gray, 6, 0.1, 20)
 
+    #corners = find_centroids(dst)
+    #corners = np.delete(corners,0, axis=0)
 
-points = corners
-top = up(points)
-print(top)
-for point in top:
-   cv2.circle(img,(int(point[0]), int(point[1])), 5, (0,0,255), -1)
+    corners_coor = []
 
-top_rs = top.reshape((-1, 1, 2)).astype(int)
-cv2.fillPoly(img, [top_rs], (0, 255, 0))
+    #converting corner's dtype
+    for corner in corners:
+        x, y = corner[0]
+        x_int, y_int = int(round(x)), int(round(y))
+        corners_coor.append([x_int, y_int])
 
-cv2.imshow('Predicted top', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # To draw the corners
+    for corner in corners_coor:
+       cv2.circle(img,(int(corner[0]), int(corner[1])), 5, (255,0,0), -1)
+
+    cv2.imshow('Found Corners', img)
+
+    points = corners_coor
+    top = up(np.array(points))
+    for point in top:
+       cv2.circle(img,(int(point[0]), int(point[1])), 5, (0, 0, 255), -1)
+
+    top_rs = top.reshape((-1, 1, 2)).astype(int)
+    cv2.fillPoly(img, [top_rs], (0, 255, 0))
+
+    cv2.imshow('Predicted top', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
